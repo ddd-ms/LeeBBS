ef1ab67ca59e626c882bad12028d9637e0a5fdac
Thomas-pc2 <2541684445@qq.com>
2024-10-25 11:19:47 +0800

    init commit

---
+++ src/main/java/com/NJU/LeeBBS/LeeBbsApplication.java
@@ -0,0 +1,13 @@
+package com.NJU.LeeBBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NJU/LeeBBS/LeeBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NJU.LeeBBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
