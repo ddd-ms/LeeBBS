b2a89964263761bb6342b3e2282275351d09e61b
Thomas-pc2 <2541684445@qq.com>
2024-10-25 11:32:08 +0800

    newer dependencies

--- src/main/java/com/NJU/SWI/LeeBBS/LeeBbsApplication.java
+++ src/main/java/com/NJU/SWI/LeeBBS/LeeBbsApplication.java
@@ -1,4 +1,4 @@
-package com.NJU.LeeBBS;
+package com.NJU.SWI.LeeBBS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/com/NJU/SWI/LeeBBS/LeeBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NJU.SWI.LeeBBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
