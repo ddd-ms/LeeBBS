8650b950d482bb23e3c4f4a5e8a883a3b2f51670
thomas <2541684445@qq.com>
2024-10-26 10:22:43 +0800

    static source - update to MyBatis

--- src/main/java/com/NJU/SWI/LeeBBS/controller/AlphaController.java
+++ src/main/java/com/NJU/SWI/LeeBBS/controller/AlphaController.java
@@ -5,12 +5,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/alpha")
+@RequestMapping("/alpha-controller")
 public class AlphaController {
     @RequestMapping("/greetings")
     @ResponseBody
@@ -45,10 +49,50 @@ public class AlphaController {
     public String getStudents(@RequestParam(value = "limit",defaultValue = "0",required = false) int limit){
         return limit + "<h1>GET students</h1>";
     }
-    @RequestMapping(path = "/student/{id}",method = RequestMethod.GET)
+    @RequestMapping(path = "/students", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name,int age){
+        System.out.println(name + " " + age);
+        return "success";
+    }
+    @RequestMapping(path = "/students/{id}",method = RequestMethod.GET)
     @ResponseBody
     public String getStudent(@PathVariable("id") int id){
 //        System.out.println("met get id");
         return id + "<h1>GET student</h1>";
+    }
+
+//    Response to Browser with HTML
+    @RequestMapping(path = "/teacher",method = RequestMethod.GET)
+    public ModelAndView getTeacher(@RequestParam(value = "id",required = false,defaultValue = "0") int id){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("id",id);
+        mav.addObject("name","Lee");
+        mav.addObject("age",18);
+        mav.setViewName("/demo/view");
+        return mav;
+    }
+
+    @RequestMapping(value = "/school", method =RequestMethod.GET)
+    public String getSchool(Model model){
+        model.addAttribute("name","NJU");
+
+        return "/demo/view";
+    }
+
+    // Response to Browser with JSON
+    @RequestMapping("/emp")
+    @ResponseBody
+    public Map<String,Object> getEmp(){
+        return Map.of("name","Lee","age",18);
+    }
+
+    @RequestMapping("/getAllEmps")
+    @ResponseBody
+    public List<Map<String,Object>> getAllEmps(){
+        return List.of(
+                Map.of("name","L1","age",22),
+                Map.of("name","L2","age",28)
+        );
     }
 }
