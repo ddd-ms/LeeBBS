c09714b83355b88587ce0f3ca63d9b3edbcea315
Thomas-pc2 <2541684445@qq.com>
2024-11-01 13:18:50 +0800

    forbiddenFilter ignored

---
+++ src/main/java/com/NJU/SWI/LeeBBS/util/ForbiddenFilter.java
@@ -0,0 +1,14 @@
+package com.NJU.SWI.LeeBBS.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ForbiddenFilter {
+    private static final Logger logger = LoggerFactory.getLogger(ForbiddenFilter.class);
+
+//  前缀树
+    private class TrieNode{
+        private boolean isKeywordEnd = false;
+}
+}
